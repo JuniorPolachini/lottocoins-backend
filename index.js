53e36061a81233ce29081f6da40f5899acd9ee77
JuniorPolachini <alceupolachinijunior@gmail.com>
2026-01-06 20:13:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("API running on port", port));
+
+import { pool } from "./db.js";
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ ok: true, db_time: result.rows[0] });
+  } catch (e) {
+    res.status(500).json({ ok: false, error: e.message });
+  }
+});
