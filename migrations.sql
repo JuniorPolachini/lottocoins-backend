da59fcf39bdb94634b1dc1b444b1da6809b1b1c4
JuniorPolachini <alceupolachinijunior@gmail.com>
2026-01-06 21:35:07 -0300

    Update migrations.sql

--- migrations.sql
+++ migrations.sql
@@ -1,34 +1,37 @@
 ALTER TABLE users
-ADD COLUMN IF NOT EXISTS balance NUMERIC(12,2) DEFAULT 0 NOT NULL;
+ADD COLUMN balance NUMERIC(12,2) DEFAULT 0 NOT NULL;
 
 CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  full_name TEXT NOT NULL,
-  cpf VARCHAR(14) NOT NULL UNIQUE,
-  birth_date DATE NOT NULL,
-  email TEXT NOT NULL UNIQUE,
-  password_hash TEXT NOT NULL,
-  whatsapp TEXT NOT NULL,
-  tibia_character TEXT NOT NULL,
-  cep VARCHAR(12),
-  street TEXT,
-  number TEXT,
-  neighborhood TEXT,
-  city TEXT,
-  state TEXT,
-  country TEXT,
-  accepted_terms BOOLEAN DEFAULT false,
-  created_at TIMESTAMP DEFAULT NOW()
+    id SERIAL PRIMARY KEY,
+    full_name TEXT NOT NULL,
+    cpf VARCHAR(14) NOT NULL UNIQUE,
+    birth_date DATE NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    whatsapp TEXT NOT NULL,
+    tibia_character TEXT NOT NULL,
+    cep VARCHAR(12),
+    street TEXT,
+    number TEXT,
+    neighborhood TEXT,
+    city TEXT,
+    state TEXT,
+    country TEXT,
+    accepted_terms BOOLEAN DEFAULT false,
+    created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS transactions (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
-  amount NUMERIC(12,2) NOT NULL,
-  type VARCHAR(20) NOT NULL, -- deposit, bet, prize
-  description TEXT,
-  created_at TIMESTAMP DEFAULT NOW()
-);
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    amount NUMERIC(12,2) NOT NULL,
+    type TEXT NOT NULL,         -- deposit, bet, prize, ajuste, etc
+    source TEXT,                -- tibia char, aposta, admin, etc
+    created_at TIMESTAMP DEFAULT NOW(),
 
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+        REFERENCES users(id)
+);
 
 
