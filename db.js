e5d5508eeb8186011d3a579faa69e5e6aef51258
JuniorPolachini <alceupolachinijunior@gmail.com>
2026-01-06 22:33:34 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
-import pg from "pg";
-const { Pool } = pg;
+import pkg from "pg";
+const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 });
